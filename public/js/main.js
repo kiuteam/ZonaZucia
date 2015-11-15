822a89e88ac990cd90f6b88bb792f1c329c60565
Rodrigo Quelca Sirpa <rodrigoquelcasirpa@MacBook-Pro-de-Rodrigo.local>
2015-11-15 11:04:05 -0400

    merge with end points

--- public/js/main.js
+++ public/js/main.js
@@ -8,20 +8,139 @@ window.collections = {};
 window.views = {};
 
 $(document).ready(function(){/* off-canvas sidebar toggle */
-   console.log("mail.js loaded");
+   console.log("main.js loaded");
    window.views.app = new Z2.Views.App( $('body') );
 
     window.collections.posts = new Z2.Collections.Posts();
     window.collections.posts.on('add',function(model){
         console.log('se ha agregado un modelo', model.toJSON());
         // agregar nuevas vistas de post aqui
+
         var view = new Z2.Views.Post({model: model});
         view.render();
         view.$el.prependTo('.posts');
     });
     window.collections.posts.fetch();
 
+    $('#mainsubmit').click(function(){
+        var locationData = {
+            latitude: window.position.latitude,
+            longitude: window.position.longitude
+        };
+
+        jQuery.ajax({
+            url: 'http://localhost:3000/api/location',
+            data: locationData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST',
+            success: function (data) {
+                if(data.file) {
+                    var imggoogleID = data.file._id,
+                        zonaName = $('#zonaName').val(),
+                        description = $('#description').val(),
+                        userID = "5647bcd51dec3c0c76c31bcf",
+                        userName =  "Juan Perez",
+                        userEmail = "juan@aaa.com",
+                        imgID = "";
+                    var dataFile = new FormData();
+                    jQuery.each(jQuery('#inputFile')[0].files, function(i, file) {
+                        dataFile.append('image', file);
+                    });
+                    jQuery.ajax({
+                        url: 'http://localhost:3000/api/upload',
+                        data: dataFile,
+                        cache: false,
+                        contentType: false,
+                        processData: false,
+                        type: 'POST',
+                        success: function(data){
+                             imgID = data.file._id || '';
+                            var data = {
+                                //"zonaId":"5647bcd51dec3c0c76c31bcf",
+                                zonaName: zonaName,
+                                "description": description,
+                                "user":{
+                                    "userId": userID,
+                                    "name": userName,
+                                    "email": userEmail
+                                },
+                                "images":{
+                                    "locations":{
+                                        "imageId": imggoogleID,
+                                        "latitude": window.position.latitude,
+                                        "longitude": window.position.longitude
+                                    },
+                                    "image": imgID
+                                }
+                            };
+                            $.ajax({
+                                type: "POST",
+                                url: "http://localhost:3000/api/post",
+                                data: JSON.stringify(data),
+                                beforeSend: function (xhrObj) {
+                                    xhrObj.setRequestHeader("Content-Type", "application/json");
+                                    //xhrObj.setRequestHeader("Accept","application/json");
+                                },
+                                success: function (data) {
+                                    console.log('almacenamiento exitoso');
+                                }
+                            });
+
+                        }
+                    });
+
+
+
+                }
+
+            }
+
+
+        });
+
+
+
+    //
+
+    //    console.log(data);
+    //    console.log('mainbutton  clic event');
+
+    //
+    //
+    //    });
+    //
+    });
+    setupMap();
 });
+
+window.setupMap = function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            window.map = new GMap2(document.getElementById("myMapContainerId"));
+            console.log(position);
+            map.setCenter(new GLatLng(position.coords.latitude, position.coords.longitude), 16);
+            map.addControl(new GLargeMapControl3D());
+            map.addControl(new GMapTypeControl());
+
+            var point = new GPoint(position.coords.longitude, position.coords.latitude);
+            var marker = new GMarker(point);  // Create the marker
+            map.addOverlay(marker);
+            // And open some infowindow, with some HTML text in it
+            marker.openInfoWindowHtml(
+                '<strong>Usted esta aqui. </strong>'
+            );
+            window.position = position;
+        });
+
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+
+
+
+};
 
 /*
  metadata
--- public/js/views/app.js
+++ public/js/views/app.js
@@ -1,7 +1,7 @@
 Z2.Views.App = Backbone.View.extend({
     events: {
         "click .publica" : "showForm",
-        "click #mainSubmit": "createPost"
+        "click .mainbutton": "createPost"
 
     },
     initialize: function($el){
@@ -11,29 +11,30 @@ Z2.Views.App = Backbone.View.extend({
         //this.$el.find('Form').show();
     },
     createPost: function(e){
+        console.log('createPost');
         e.preventDefault();
         //var title = $('input[name=titulo]').val(),
         //    username = "rodrigo quelca",
         //    description = $('input[name=description]').val();
-        var data = {
-            "zonaId":"5647bcd51dec3c0c76c31bcf",
-                "description":"dhkjlsdalkjvksjav",
-                "user":{
-                "userId":"5647bcd51dec3c0c76c31bcf",
-                    "name":"patito",
-                    "email":"a@a.com"
-            },
-            "images":{
-                "locations":{
-                    "imageId":"5647bcd51dec3c0c76c31bcf",
-                        "latitude":30,
-                        "longitude":50
-                },
-                "image":"5647bcd51dec3c0c76c31bcf"
-            }
-        };
-        var model = new Z2.Models.Post(data);
-        model.save();
+        //var data = {
+        //    "zonaId":"5647bcd51dec3c0c76c31bcf",
+        //        "description":"dhkjlsdalkjvksjav",
+        //        "user":{
+        //        "userId":"5647bcd51dec3c0c76c31bcf",
+        //            "name":"patito",
+        //            "email":"a@a.com"
+        //    },
+        //    "images":{
+        //        "locations":{
+        //            "imageId":"5647bcd51dec3c0c76c31bcf",
+        //                "latitude":30,
+        //                "longitude":50
+        //        },
+        //        "image":"5647bcd51dec3c0c76c31bcf"
+        //    }
+        //};
+        //var model = new Z2.Models.Post(data);
+        //model.save();
 
     }
 });
